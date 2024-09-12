33a95223c957378b21ea866a6a70e5a747452995
Naw-R <rowannasser14@gmail.com>
2024-09-12 14:00:15 -0400

    Implemented the greatest record of all time

--- Topics/2-Instructions/Drawing/js/script.js
+++ Topics/2-Instructions/Drawing/js/script.js
@@ -1,24 +1,46 @@
 /**
- * Title of Project
- * Author Name
+ * The Greatest Record of all time
+ * Rowan Nasser
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Display the greatest record of all time
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create the square canvas
 */
 function setup() {
-
+    createCanvas(640,640);
+    
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Displays the Record
 */
 function draw() {
+    background(150);
+
+    // The main part of the record
+    push();       // Beggining of drawing
+    fill(255,0,0);
+    stroke(255);
+    ellipse(320, 320, 480, 480);
+    pop();      //End of drawing
+
+    // The label on the record
+    push();
+    fill("white");
+    noStroke();
+    ellipse(320, 320, 140,140);
+    pop();
+
+    // The hole in the record
+    push();
+    fill("#000000");
+    noStroke();
+    ellipse(320, 320, 20);
+    pop();
 
 }
