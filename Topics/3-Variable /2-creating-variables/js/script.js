3437484a3d25ba04e5cf472075a0cec38cbd132d
Naw-R <rowannasser14@gmail.com>
2024-09-22 10:13:03 -0400

    started the creating variable module

---
+++ Topics/3-Variable /2-creating-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * creating variables
+ * Rowan Nasser     
+ * 
+ * Experimenting with creating variables
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
