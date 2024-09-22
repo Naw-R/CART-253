d1b442fd4af460216ab017e95a381c716d2a5b1a
Naw-R <rowannasser14@gmail.com>
2024-09-22 10:00:44 -0400

    started the introducing variables project

---
+++ Topics/3-Variable /Introducing-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing Variables
+ * Rowan Nasser
+ * 
+ * Learning what variables is and does
+ */
+
+"use strict";
+
+/**
+ * Create a canvas
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
