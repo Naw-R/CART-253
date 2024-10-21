9b028ddf62ad396e147b8a4297aee069ab50b020
Naw-R <rowannasser14@gmail.com>
2024-10-21 15:14:54 -0400

    Edit keyboard controls with colors

--- Topics/5-Events/4-Keyboard-events/js/script.js
+++ Topics/5-Events/4-Keyboard-events/js/script.js
@@ -21,6 +21,11 @@ const ball = {
         white: "#ffffff",
         red: "#ff0000",
         blue: "#0000ff"
+    },
+    // keys
+    keys:{
+        redKey : 82,
+        blueKey : 66,
     }
 }
 
@@ -43,4 +48,25 @@ function draw() {
     fill(ball.fill);
     ellipse(ball.x, ball.y, ball.size);
     pop();
+}
+
+/**
+ * When keyboard is pressed, change ball color
+ */
+function keyPressed(event){
+    if (event.keyCode === ball.keys.redKey) {
+        ball.fill = ball.fills.red;
+    }
+    else if (event.keyCode === ball.keys.blueKey) {
+        ball.fill = ball.fills.blue;
+    }
+}
+
+/**
+ * When keyboard is released, change ball color
+ */
+function keyReleased(event){
+    if (event.keyCode === ball.keys.redKey || event.keyCode === ball.keys.blueKey) {
+        ball.fill = ball.fills.white;
+    }
 }
