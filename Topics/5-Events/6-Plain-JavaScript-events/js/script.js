fe600dc2accc1d52c950e1c40f5657c88d3a1813
Naw-R <rowannasser14@gmail.com>
2024-10-21 16:12:15 -0400

    Started the plain javascript project

---
+++ Topics/5-Events/6-Plain-JavaScript-events/js/script.js
@@ -0,0 +1,23 @@
+/**
+ *  Plain JavaScript Events
+ * Rowan NAsser
+ * 
+ * Experimneting with event handling in plain javascript
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
